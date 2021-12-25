d44980d387a8632437fad907f95a1ca530d7d0dc
Brock Brown <brockrbrown@gmail.com>
2021-12-25 15:36:00 -0500

    some randomicamization

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flask import render_template
+from random import choice, randint
 
 app = Flask(__name__)
 
@@ -7,3 +8,16 @@ app = Flask(__name__)
 @app.route("/")
 def hello_world():
     return render_template("index.html")
+
+def random_color():
+    a, b, c = (randint(30,255), randint(30,255), randint(30,255))
+    return f"({a}, {b}, {c})"
+
+def random_tidbit():
+    sayings = [
+    	"git along little doggy",
+    	"hello out there :)",
+    	"hello ma baby hello ma honey hello ma ragtime gal"]
+    return choice(sayings)
+
+app.jinja_env.globals.update(random_tidbit=random_tidbit, random_color=random_color)
