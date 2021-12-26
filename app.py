6e12ff92c5be5ddff7202dee9d29e167872d1de5
Brock Brown <brockrbrown@gmail.com>
2021-12-26 11:53:31 -0500

    added a new year forecast

--- app.py
+++ app.py
@@ -30,4 +30,19 @@ def random_tidbit():
     	"hello ma baby hello ma honey hello ma ragtime gal"]
     return choice(sayings)
 
+def your_new_year_forecast():
+    output = "Your new year's forecast: "
+    outcomes = ["ABSOLUTE HOGWASH",
+    	"alright i guess",
+    	"you will get a spik up ur butt",
+    	"simply stupendous nothing bad could possibly happen",
+    	"probably become a hermit or something",
+    	"you will be promoted to eagle scout",
+    	"you will be bit by monkeys more than you have in previous years",
+    	"CERTAIN DEATH"]
+    output += choice(outcomes)
+    return output
+
+
 app.jinja_env.globals.update(random_tidbit=random_tidbit, random_color=random_color)
+app.jinja_env.globals.update(your_new_year_forecast = your_new_year_forecast)
